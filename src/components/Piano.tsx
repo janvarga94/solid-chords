33488fa03dd2cc565754cf1098fadd8ffd9e2c76
Jan <janvarga94@gmail.com>
2024-09-03 18:04:16 +0200

    very good

---
+++ src/components/Piano.tsx
@@ -0,0 +1,159 @@
+import { batch, For } from "solid-js";
+import { createStore } from "solid-js/store";
+import { createKeyViewmodel } from "~/bussiness/keyViewmodel";
+import {
+    ChordType,
+    getAllChordsForSeminote,
+    semitoneToToneAndOctave,
+} from "~/bussiness/notes";
+import { play } from "~/bussiness/player";
+import { Key } from "~/models/viewModel";
+
+export interface ChordNameAndNotes {
+    /**
+     * C4, C#4 ...
+     */
+    name: string;
+    notes: string[];
+}
+
+export function Piano(props: {
+    chordTypes: ChordType[];
+    onChordPlay: (
+        chord: ChordNameAndNotes,
+        matchingChords: ChordNameAndNotes[]
+    ) => void;
+}) {
+    let [keys, setKeys] = createStore(createKeyViewmodel());
+
+    let playKeyWithMatchingChord = (key: Key) => {
+        let chords = getAllChordsForSeminote(key.seminote, props.chordTypes);
+        let specialChords = chords; /*.filter(
+            (c) => c!.matchingFifthSeminotes >= 3
+        );*/
+        let randomChord =
+            specialChords[Math.floor(Math.random() * specialChords.length)]!;
+
+        let chordsOctaveOffset = 1;
+        let lowestSeminote =
+            randomChord.seminotes[
+                Math.floor(Math.random() * randomChord.seminotes.length)
+            ];
+        let mappedNotes = randomChord.seminotes.map((semi) => {
+            // randomize order of note in chord
+            return semitoneToToneAndOctave(
+                semi,
+                semi >= lowestSeminote
+                    ? key.octave - chordsOctaveOffset
+                    : key.octave + 1 - chordsOctaveOffset
+            );
+        });
+
+        play(mappedNotes);
+        play([semitoneToToneAndOctave(key.seminote, key.octave + 1)]);
+
+        props.onChordPlay(
+            { name: randomChord.name, notes: mappedNotes },
+            specialChords.map((ch) => {
+                return { name: ch!.name, notes: [] };
+            })
+        );
+        batch(() => {
+            setKeys(
+                (storeKey) => storeKey.isPlaying === true,
+                "isPlaying",
+                false
+            );
+            setKeys(
+                (storeKey) => mappedNotes.includes(storeKey.noteAndOctave),
+                "isPlaying",
+                true
+            );
+            setKeys(
+                (storeKey) =>
+                    storeKey.noteAndOctave ===
+                    semitoneToToneAndOctave(key.seminote, key.octave),
+                "isPlaying",
+                true
+            );
+        });
+    };
+
+    let playKey = (key: Key) => {
+        play([semitoneToToneAndOctave(key.seminote, key.octave)]);
+
+        batch(() => {
+            setKeys(
+                (storeKey) => storeKey.isPlaying === true,
+                "isPlaying",
+                false
+            );
+            setKeys(
+                (storeKey) => storeKey.noteAndOctave === key.noteAndOctave,
+                "isPlaying",
+                true
+            );
+        });
+    };
+
+    return (
+        <div style="height:120px; position:relative; overflow-x:scroll;scrollbar-width: thin; margin-top: 15px;">
+            <For each={keys}>
+                {(key) => (
+                    <div
+                        oncontextmenu={(e) => {
+                            playKey(key);
+                            e.preventDefault();
+                        }}
+                        onMouseDown={(e) =>
+                            e.button === 0 && playKeyWithMatchingChord(key)
+                        }
+                        style={{
+                            border: "solid 1px black",
+                            display: "inline-flex",
+                            "justify-content": "space-between",
+                            "flex-direction": "column",
+                            "background-color": key.isPlaying
+                                ? key.isBlack
+                                    ? "rgb(155,155,0)"
+                                    : "yellow"
+                                : key.isBlack
+                                  ? "black"
+                                  : "white",
+                            color: key.isBlack ? "white" : "black",
+                            height: key.isBlack ? "70px" : "100px",
+                            position: "absolute",
+                            left: key.left + "px",
+                            width: key.width + "px",
+                            cursor: "default",
+                            "z-index": key.isBlack ? "12332" : undefined,
+                            "border-radius": "2px",
+                        }}
+                    >
+                        <div style="height:20px; "></div>
+                        <div style="height:25px; ">
+                            <div
+                                style={{
+                                    "font-size": key.isBlack ? "5px" : "8px",
+                                    display: "flex",
+                                    "justify-content": "center",
+                                }}
+                            >
+                                {key.noteAndOctave}
+                            </div>
+                            <div
+                                style={{
+                                    "font-size": key.isBlack ? "8px" : "15px",
+                                    display: "flex",
+                                    "justify-content": "center",
+                                }}
+                            >
+                                {key.myChordMark}
+                            </div>
+                        </div>
+                    </div>
+                )}
+            </For>
+        </div>
+    );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,14 @@
 import { createStore } from "solid-js/store";
-import { batch, createSignal, For } from "solid-js";
-import {
-    ChordType,
-    getAllChordsForSeminote,
-    semitoneToToneAndOctave,
-} from "~/bussiness/notes.js";
+import { createSignal, For, Show } from "solid-js";
+import { ChordType } from "~/bussiness/notes.js";
 import { ChordBadge, MiniChordBadge } from "~/components/ChordBadges.jsx";
-import { createKeyViewmodel } from "~/bussiness/keyViewmodel.js";
-import { Key } from "~/models/viewModel.js";
 import { ChordPlayMode, play } from "~/bussiness/player";
+import { ChordNameAndNotes, Piano } from "~/components/Piano";
 
 export default function Home() {
-    let [currentChord, setCurrentChord] = createSignal("");
-    let [currentChordNotes, setCurrentChordNotes] = createSignal<string[]>([]);
-    let [matchingChords, setMatchingChords] = createSignal<
-        { name: string; notes: string[] }[] | undefined
-    >();
+    let [currentChord, setCurrentChord] = createSignal<ChordNameAndNotes>();
+    let [matchingChords, setMatchingChords] =
+        createSignal<ChordNameAndNotes[]>();
 
     let [chordPlayMode, setChordPlayMode] =
         createSignal<ChordPlayMode>("together");
@@ -34,79 +27,6 @@ export default function Home() {
         "maj7",
         "min7",
     ];
-
-    let [keys, setKeys] = createStore(createKeyViewmodel());
-
-    let playKeyWithMatchingChord = (key: Key) => {
-        let chords = getAllChordsForSeminote(key.seminote, chordTypes);
-        let specialChords = chords; /*.filter(
-            (c) => c!.matchingFifthSeminotes >= 3
-        );*/
-        let randomChord =
-            specialChords[Math.floor(Math.random() * specialChords.length)]!;
-
-        let chordsOctaveOffset = 1;
-        let lowestSeminote =
-            randomChord.seminotes[
-                Math.floor(Math.random() * randomChord.seminotes.length)
-            ];
-        let mappedNotes = randomChord.seminotes.map((semi) => {
-            // randomize order of note in chord
-            return semitoneToToneAndOctave(
-                semi,
-                semi >= lowestSeminote
-                    ? key.octave - chordsOctaveOffset
-                    : key.octave + 1 - chordsOctaveOffset
-            );
-        });
-
-        play(mappedNotes);
-        play([semitoneToToneAndOctave(key.seminote, key.octave + 1)]);
-
-        setCurrentChord(randomChord.name);
-        setCurrentChordNotes(mappedNotes);
-        setMatchingChords(
-            specialChords.map((ch) => {
-                return { name: ch!.name, notes: [] };
-            })
-        );
-        batch(() => {
-            setKeys(
-                (storeKey) => storeKey.isPlaying === true,
-                "isPlaying",
-                false
-            );
-            setKeys(
-                (storeKey) => mappedNotes.includes(storeKey.noteAndOctave),
-                "isPlaying",
-                true
-            );
-            setKeys(
-                (storeKey) =>
-                    storeKey.noteAndOctave ===
-                    semitoneToToneAndOctave(key.seminote, key.octave),
-                "isPlaying",
-                true
-            );
-        });
-    };
-
-    let playKey = (key: Key) => {
-        play([semitoneToToneAndOctave(key.seminote, key.octave)]);
-
-        batch(() => {
-            setKeys(
-                (storeKey) => storeKey.isPlaying === true,
-                "isPlaying",
-                false
-            );
-            setKeys(
-                (storeKey) => storeKey.noteAndOctave === key.noteAndOctave,
-                "isPlaying",
-                true
-            );
-        });
-    };
 
     let chordTypeChange = (isChecked: boolean, chordType: ChordType) => {
         if (isChecked) {
@@ -141,80 +61,25 @@ export default function Home() {
                     </span>
                 ))}
             </div>
-            <div
-                id="keys"
-                style="height:120px; position:relative; overflow-x:scroll;scrollbar-width: thin; margin-top: 15px;"
-            >
-                <For each={keys}>
-                    {(key) => (
-                        <div
-                            oncontextmenu={(e) => {
-                                playKey(key);
-                                e.preventDefault();
-                            }}
-                            onMouseDown={(e) =>
-                                e.button === 0 && playKeyWithMatchingChord(key)
-                            }
-                            style={{
-                                border: "solid 1px black",
-                                display: "inline-flex",
-                                "justify-content": "space-between",
-                                "flex-direction": "column",
-                                "background-color": key.isPlaying
-                                    ? key.isBlack
-                                        ? "rgb(155,155,0)"
-                                        : "yellow"
-                                    : key.isBlack
-                                      ? "black"
-                                      : "white",
-                                color: key.isBlack ? "white" : "black",
-                                height: key.isBlack ? "70px" : "100px",
-                                position: "absolute",
-                                left: key.left + "px",
-                                width: key.width + "px",
-                                cursor: "default",
-                                "z-index": key.isBlack ? "12332" : undefined,
-                                "border-radius": "2px",
-                            }}
-                        >
-                            <div style="height:20px; "></div>
-                            <div style="height:25px; ">
-                                <div
-                                    style={{
-                                        "font-size": key.isBlack
-                                            ? "5px"
-                                            : "8px",
-                                        display: "flex",
-                                        "justify-content": "center",
-                                    }}
-                                >
-                                    {key.noteAndOctave}
-                                </div>
-                                <div
-                                    style={{
-                                        "font-size": key.isBlack
-                                            ? "8px"
-                                            : "15px",
-                                        display: "flex",
-                                        "justify-content": "center",
-                                    }}
-                                >
-                                    {key.myChordMark}
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </For>
-            </div>
 
-            <div style="margin-top:15px;display: flex; justify-content:center">
-                <ChordBadge
-                    chordName={currentChord()}
-                    onClick={() => {
-                        play(currentChordNotes());
-                    }}
-                ></ChordBadge>
-            </div>
+            <Piano
+                chordTypes={chordTypes}
+                onChordPlay={(chord, matchingChords) => {
+                    setCurrentChord(chord);
+                    setMatchingChords(matchingChords);
+                }}
+            ></Piano>
+
+            <Show when={currentChord()}>
+                <div style="margin-top:15px;display: flex; justify-content:center">
+                    <ChordBadge
+                        chordName={currentChord()?.name!}
+                        onClick={() => {
+                            play(currentChord()?.notes!);
+                        }}
+                    ></ChordBadge>
+                </div>
+            </Show>
             <div style="margin-top: 10px; font-size:10px; display: flex; justify-content:center">
                 <i>matching chords</i>
             </div>
